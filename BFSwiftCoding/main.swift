72ffcdd57b3dbe29aaaadc926a593613367f88df
BFAlex <bfalex@BFAlexdeMac.local>
2018-04-04 00:38:43 +0800

    Initial Commit

---
+++ BFSwiftCoding/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BFSwiftCoding
+//
+//  Created by BFAlex on 2018/4/4.
+//  Copyright © 2018年 BFAlex. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
